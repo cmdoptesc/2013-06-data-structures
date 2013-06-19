ac23f746d13db0f9788659c913349c72e2d4938d
Hack Reactor Students <github@hackreactor.com>
2013-06-19 14:43:25 -0700

    write functional stack.push() and stack.pop()

--- src/functional/stack.js
+++ src/functional/stack.js
@@ -2,15 +2,21 @@ var makeStack = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
   var stack = {};
 
   stack.push = function(value){
+    stack[size] = value;
+    size += 1;
   };
 
   stack.pop = function(){
+    var temp = stack[size-1];
+    delete stack[size-1];
+    size -= 1;
+    return temp;
   };
 
   stack.size = function(){
