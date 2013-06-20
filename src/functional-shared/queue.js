e6ebebf13c1c28ae88e8d1de58d13e18ea5c782c
Hack Reactor Students <github@hackreactor.com>
2013-06-19 20:46:57 -0700

    Implement functional-shared/queue.js in rememberance of Ensign Ricky.

--- src/functional-shared/queue.js
+++ src/functional-shared/queue.js
@@ -1,3 +1,36 @@
 var makeQueue = function(){
-	// Hey! Copy your code from src/functional/queue.js and paste it here
+  var queue = {
+    front: 0,
+    back: 0,
+    storage: {},
+    enqueue: makeQueue.queueMethods.enqueue,
+    dequeue: makeQueue.queueMethods.dequeue,
+    size: makeQueue.queueMethods.size
+  };
+  return queue;
+};
+
+
+makeQueue.queueMethods = {
+
+  enqueue: function(value) {
+    if(typeof value !== 'undefined') {
+      this.storage[this.back] = value;
+      this.back += 1;
+    }
+  },
+
+  dequeue: function() {
+    if(this.front !== this.back) {
+      var temp = this.storage[this.front];
+      delete this.storage[this.front];
+      this.front += 1;
+      return temp;
+    }
+  },
+
+  size: function() {
+    return this.back-this.front;
+  }
+
 };
