756347fa36e81cd06d6d3950756c24df7d35c3d7
Hack Reactor Students <github@hackreactor.com>
2013-06-20 14:24:42 -0700

    middle of fix for psuedoclassical

--- src/pseudoclassical/queue.js
+++ src/pseudoclassical/queue.js
@@ -1,3 +1,80 @@
-var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+
+var Queue = function  () {
+  // this = Object.create(Person.prototype)
+  var root = this;
+
+  root = {
+    front: 0,
+    back: 0,
+    storage: {}
+  };
+  // return this
+
 };
+
+Queue.prototype.methods = {
+
+  enqueue: function(value) {
+    if(typeof value !== 'undefined') {
+      this.storage[this.back] = value;
+      this.back += 1;
+    }
+  },
+
+  dequeue: function() {
+    if(this.front !== this.back) {
+      var temp = this.storage[this.front];
+      delete this.storage[this.front];
+      this.front += 1;
+      return temp;
+    }
+  },
+
+  size: function() {
+    return this.back-this.front;
+  }
+
+
+};
+
+
+
+
+
+
+
+
+
+// var Queue = function() {
+//   var bigQueue = Object.create(Queue.queueMethods);
+//   return bigQueue;
+// };
+
+// Queue.queueMethods = {
+
+// //properties
+//   front: 0,
+//   back: 0,
+//   storage: {},
+
+//   enqueue: function(value) {
+//     if(typeof value !== 'undefined') {
+//       this.storage[this.back] = value;
+//       this.back += 1;
+//     }
+//   },
+
+//   dequeue: function() {
+//     if(this.front !== this.back) {
+//       var temp = this.storage[this.front];
+//       delete this.storage[this.front];
+//       this.front += 1;
+//       return temp;
+//     }
+//   },
+
+//   size: function() {
+//     return this.back-this.front;
+//   }
+
+// };
--- src/pseudoclassical/stack.js
+++ src/pseudoclassical/stack.js
@@ -1,3 +1,30 @@
-var Queue = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+var Stack = function() {
+  var bigStackz = Object.create(Stack.stackMethods);
+
+  bigStackz.storage = {};
+  bigStackz.bigMoneySize = 0;
+
+  return bigStackz;
+};
+
+Stack.stackMethods = {
+
+  push: function(value){
+    if (typeof value !== 'undefined') {
+      this.storage[this.bigMoneySize] = value;
+      this.bigMoneySize += 1;
+    }
+  },
+
+  pop: function(){
+    var temp = this.storage[this.bigMoneySize-1];
+    delete this.storage[this.bigMoneySize-1];
+    if(this.bigMoneySize>0) { this.bigMoneySize -= 1; }
+    return temp;
+  },
+
+  size: function(){
+    return this.bigMoneySize;
+  }
+
 };
