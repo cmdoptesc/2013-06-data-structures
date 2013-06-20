be45b0d2ec5dfdb3348b861e5b46c3e194bf6c8b
phillipalexander <git@phillipalexander.io>
2013-06-20 14:50:15 -0700

    refactor src/functional/queue.js to use object literal notation

--- src/functional/queue.js
+++ src/functional/queue.js
@@ -1,34 +1,29 @@
-var makeQueue = function(){
-  // Use an object with numeric keys to store values
+var makeQueue = function () {
   var storage = {};
-
-  var size; // Hint: set an initial value here
-
-  // Implement the methods below
-  var queue = {};
-
-  // to keep track of relative ends of the queue (like human-centipede ))<>(()
+  var size;
   var front = 0;
   var back = 0;
 
-  queue.enqueue = function(value){
-    if(typeof value !== 'undefined') {
-      storage[back] = value;
-      back += 1;
-    }
-  };
+  var queue = {
+    enqueue: function (value) {
+      if (typeof value !== 'undefined') {
+        storage[back] = value;
+        back += 1;
+      }
+    },
 
-  queue.dequeue = function(){
-    if(front !== back) {
-      var temp = storage[front];
-      delete storage[front];
-      front += 1;
-      return temp;
-    }
-  };
+    dequeue: function () {
+      if (front !== back) {
+        var temp = storage[front];
+        delete storage[front];
+        front += 1;
+        return temp;
+      }
+    },
 
-  queue.size = function(){
-    return back-front;
+    size: function () {
+      return back - front;
+    }
   };
 
   return queue;
