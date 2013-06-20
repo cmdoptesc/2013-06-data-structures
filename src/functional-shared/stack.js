71bea75cb3dc764a051144e269f87326d082fc71
Hack Reactor Students <github@hackreactor.com>
2013-06-19 20:18:47 -0700

    Implement functional-shared/stack.js. BIGMONEYSIZE!

--- src/functional-shared/stack.js
+++ src/functional-shared/stack.js
@@ -1,3 +1,33 @@
 var makeStack = function(){
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+  var stack = {
+    push: makeStack.stackMethods.push,
+    pop: makeStack.stackMethods.pop,
+    size: makeStack.stackMethods.size,
+    storage: {},
+    bigMoneySize: 0
+  };
+
+  return stack;
+};
+
+makeStack.stackMethods = {
+
+  push: function(value){
+    if (typeof value !== 'undefined') {
+      this.storage[this.bigMoneySize] = value;
+      this.bigMoneySize += 1;
+    }
+  },
+
+  pop: function(){
+    var temp = this.storage[this.bigMoneySize-1];
+    delete this.storage[this.bigMoneySize-1];
+    if(this.bigMoneySize>0) { this.bigMoneySize -= 1; }
+    return temp;
+  },
+
+  size: function(){
+    return this.bigMoneySize;
+  }
+
 };
