1629a916d745d392aa3d554d264445a574eb752b
Hack Reactor Students <github@hackreactor.com>
2013-06-19 21:22:52 -0700

    implemented a totally brototypal version of src/prototypal/stack.js ...bro

--- src/prototypal/stack.js
+++ src/prototypal/stack.js
@@ -1,3 +1,30 @@
 var Stack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var bigStackz = Object.create(Stack.stackMethods);
+
+  bigStackz.storage = {};
+  bigStackz.bigMoneySize = 0;
+
+  return bigStackz;
+};
+
+Stack.stackMethods = {
+
+  push: function(value){
+    if (typeof value !== 'undefined') {
+      this.storage[this.bigMoneySize] = value;
+      this.bigMoneySize += 1;
+    }
+  },
+
+  pop: function(){
+    var temp = this.storage[this.bigMoneySize-1];
+    delete this.storage[this.bigMoneySize-1];
+    if(this.bigMoneySize>0) { this.bigMoneySize -= 1; }
+    return temp;
+  },
+
+  size: function(){
+    return this.bigMoneySize;
+  }
+
 };
