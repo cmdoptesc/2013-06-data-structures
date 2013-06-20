0455e32011d6f8ad7e93e4d456d89b19d49cd5d2
Hack Reactor Students <github@hackreactor.com>
2013-06-19 21:39:17 -0700

    SHIT ===> GOLD (implement src/prototypal/queue.js)

--- src/prototypal/queue.js
+++ src/prototypal/queue.js
@@ -1,3 +1,35 @@
 var Queue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+  var bigQueue = Object.create(Queue.queueMethods);
+  bigQueue.front = 0;
+  bigQueue.back = 0;
+  bigQueue.storage = {};
+  // THESE ARE NO LONGER NEEDED B/C THEY'RE DELAGATED ABOVE (by using Object.create)
+  // bigQueue.enqueue = makeQueue.queueMethods.enqueue,
+  // bigQueue.dequeue = makeQueue.queueMethods.dequeue,
+  // bigQueue.size = makeQueue.queueMethods.size;
+  return bigQueue;
+};
+
+Queue.queueMethods = {
+
+  enqueue: function(value) {
+    if(typeof value !== 'undefined') {
+      this.storage[this.back] = value;
+      this.back += 1;
+    }
+  },
+
+  dequeue: function() {
+    if(this.front !== this.back) {
+      var temp = this.storage[this.front];
+      delete this.storage[this.front];
+      this.front += 1;
+      return temp;
+    }
+  },
+
+  size: function() {
+    return this.back-this.front;
+  }
+
 };
